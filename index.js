471bd30947ac2d03cce8b3cab5a26cebef78e0a4
St4nd4rd <63095964+St4nd4rd@users.noreply.github.com>
2021-01-13 15:21:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function ConvertToDollar()
                 {
-                    document.converter.dollar.value = document.convert.euro.value * 1.21723
+                    document.converter.dollar.value = document.convert.euro.value * 1.21
                 }
 function ConvertToEuro() 
                 {
-                    document.convert.dollar.value = document.convert.euro.value * 1.21723
+                    document.convert.dollar.value = document.convert.euro.value * 1.21
                 }
